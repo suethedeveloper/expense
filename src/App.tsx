55215a4b5ec7aeff4b6491c9e0e1ddf0853755a1
Sue Lucas <choi.soyoung@gmail.com>
2022-03-08 20:13:27 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
     </div>
   );
 }
-*/
+
 
 function App() {
   const expenses = [
@@ -120,30 +120,51 @@ function App() {
       ></ExpenseItem>
     </div>
   );
+*/
 
-  /**
-  interface Expense {
-    id: string; 
-    title: string; 
-    amount: number; 
-    date: Date;
+interface Expense {
+  id: string; 
+  title: string; 
+  amount: number; 
+  date: Date;
 }
-    return (
+
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: "Toilet Papaer",
+      amount: 94.12,
+      date: new Date(2020, 7, 4)
+    },
+    {
+      id: 'e2',
+      title: "new TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    }
+  ]
+  return (
     <div>
       <h2>Let's get started!</h2>
       {
-        expenses.map((expense: Expense, index: number) => {
+        expenses.map((expense: Expense, index: number) => (
           <ExpenseItem 
             key={index}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           ></ExpenseItem>
-        })
+        ))
       }
     </div>
   ); 
-   */
 }
 
 export default App;
