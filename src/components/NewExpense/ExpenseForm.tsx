97e61bb4de69d01b435ed9d0a232a00c92c3da82
Sue Lucas <choi.soyoung@gmail.com>
2022-03-09 23:17:33 -0800

    4-54: Listen for change events on inputs

--- src/components/NewExpense/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-
+    //@ts-ignore
+    const [title, setTitle] = useState("");
+    const titleChangeHandler = (evt: any) => {
+        setTitle(evt.target.value);
+    }
+    //@ts-ignore
+    const [amount, setAmount] = useState("");
+    const amountChangeHandler = (evt: any) => {
+        setAmount(evt.target.value);
+    }
+    //@ts-ignore
+    const [date, setDate] = useState("");
+    const dataChangeHandler = (evt: any) => {
+        setDate(evt.target.value);
+    }
+    
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -15,6 +31,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            onChange={amountChangeHandler}
           />
         </div>
         <div className='new-expense__control'>
@@ -23,6 +40,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            onChange={dataChangeHandler}
           />
         </div>
       </div>
