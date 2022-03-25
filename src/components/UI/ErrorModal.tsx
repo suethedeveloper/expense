6ab1e923f7f6bb17437dd21c795bdc3344ef8219
Sue Lucas <choi.soyoung@gmail.com>
2022-03-24 23:47:56 -0700

    105 -working with portals

--- src/components/UI/ErrorModal.tsx
+++ src/components/UI/ErrorModal.tsx
@@ -1,12 +1,15 @@
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const ErrorModal = (props: {title: string; message: string, onConfirm: () => void}) => {
+const Backdrop = (props:  {onConfirm: () => void}) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
 
+const ModalOverlay = (props: {title: string; message: string, onConfirm: () => void}) => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}></div>
             <Card className={classes.modal}>
                 <header className={classes.header}>
                     <h2>{props.title}</h2>
@@ -18,8 +21,26 @@ const ErrorModal = (props: {title: string; message: string, onConfirm: () => voi
                     <Button onClick={props.onConfirm}>OK</Button>
                 </footer>
             </Card>
-        </div>
     );
+}
+
+const ErrorModal = (props: any) => {
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />, 
+                document.getElementById('backdrop-root')!
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay 
+                    title={props.title} 
+                    message={props.message}
+                    onConfirm={props.onConfirm}  
+                />, 
+                document.getElementById("overlay-root")!
+            )}
+        </React.Fragment>
+    )
 }
 
 export default ErrorModal;
