6e85b8decc65f11e2c1ce32c69023d62363f1590
Sue <choi.soyoung@gmail.com>
2022-04-10 16:26:57 -0700

    Initial setup

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import { AuthContextProvider } from './store/auth-context';
-
-ReactDOM.render(<AuthContextProvider><App /></AuthContextProvider>, document.getElementById('root'));
