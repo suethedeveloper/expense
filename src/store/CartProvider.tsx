e1b3aee9cfb63aa277b9de975b1187f026e80c0b
Sue <choi.soyoung@gmail.com>
2022-04-14 23:23:13 -0700

    144. Adding a Cart Reducer

--- src/store/CartProvider.tsx
+++ src/store/CartProvider.tsx
@@ -1,15 +1,38 @@
+import { useReducer } from "react";
 import { ReactChildren } from "react";
 import CartContext from "./card-context";
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+ const cartReducer = (state: {items: any, totalAmount: number}, action: any) =>  {
+     if (action.type === "ADD") {
+         const updatedItems = state.items.concat(action.item);
+         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount; 
+         return {
+             items: updatedItems,
+             totalAmount: updatedTotalAmount
+         }
+     }
+     return defaultCartState;
+ }
+
 const CartProvider = (props: {children: ReactChildren}) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
     //@ts-ignore
-    const addItemToCartHandler = (item: object) => {};
+    const addItemToCartHandler = (item: object) => {
+        dispatchCartAction({type:'ADD', item});
+    };
     //@ts-ignore
-    const removeItemToCartHandler = (id: string) => {};
+    const removeItemToCartHandler = (id: string) => {
+        dispatchCartAction({type:'REMOVE', id});
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemToCartHandler
     };
