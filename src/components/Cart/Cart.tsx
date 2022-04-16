23c9ed6f0da167f0196b8410e8ce25fd6f8610d0
Sue <choi.soyoung@gmail.com>
2022-04-16 16:45:23 -0700

    146. Outputting Cart Items

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,20 +1,45 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext, { Item } from "../../store/card-context";
+import CartItem from "./CartItem";
 
 const Cart = (props: {onClose: () => void}) => {
-    const cartItems = <ul className={classes['cart-items']}>{[
-            {id: "c1", name: "Sushi", amount: 2, price: 12.99}
-        ].map((item: any) => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+    const cartItemRemoveHandler = (id: string) => {
+        cartCtx.removeItem(id);
+    };
+    const cartItemAddHandler = (item: Item) => {
+        cartCtx.addItem({...item, amount: 1});
+    };
+
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {cartCtx.items.map(item => (
+                <CartItem
+                    id={item.id}
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
+        </ul>
+    );
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
             <div>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     )
--- src/store/card-context.tsx
+++ src/store/card-context.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+export type Item = { id: string; name: string; amount: number; price: number };
 
-const CartContext = React.createContext({
+type CartContextProps = {
+    items: Item[];
+    totalAmount: number;
+    addItem(item: Item): void;
+    removeItem(id: string): void;
+  };
+
+const CartContext = React.createContext<CartContextProps>({
     items: [],
     totalAmount: 0,
     addItem: (_item: object) => {},
