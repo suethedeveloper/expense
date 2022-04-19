f68da75da2086a680ebead3d9356b93c221c3fb6
Sue <choi.soyoung@gmail.com>
2022-04-18 21:11:33 -0700

    Initial setup

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { ReactChild } from 'react';
+import classes from './Button.module.css';
+
+const Button = (props: {
+  type: "button" | "submit" | "reset" | undefined;
+  onClick: () => void;
+  disabled: boolean;
+  children: ReactChild,
+  className: string
+}) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
