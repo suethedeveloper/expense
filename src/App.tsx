2989cde9f6cf05cad374ba56eae3133841f5ff26
Sue <choi.soyoung@gmail.com>
2022-04-21 23:05:05 -0700

    180. Handling Http Errors- part2

--- src/App.tsx
+++ src/App.tsx
@@ -39,16 +39,24 @@ function App() {
     }
   }
 
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading..</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
+        {content}
       </section>
     </React.Fragment>
   );
