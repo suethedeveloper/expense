aec846f0aa36d924b5c26cfb525d29471c5edd4b
Sue <choi.soyoung@gmail.com>
2022-04-21 22:45:28 -0700

    178. Using async / await

--- src/App.tsx
+++ src/App.tsx
@@ -6,41 +6,26 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMovieHandler() {
-    fetch("https://swapi.dev/api/films/").then(response => 
-      response.json()
-    ).then(data => {
-      const transformedMovies = data.results.map((movieData: {
-        episode_id: number;
-        title:string;
-        opening_crawl: string;
-        release_date: string;
-      }) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date       
-        }
-      })
-      setMovies(transformedMovies);
-    });
-  }
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
 
+    const transformedMovies = data.results.map((movieData: {
+      episode_id: number;
+      title:string;
+      opening_crawl: string;
+      release_date: string;
+    }) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date       
+      }
+    })
+    setMovies(transformedMovies);
+  }
+  
   return (
     <React.Fragment>
       <section>
