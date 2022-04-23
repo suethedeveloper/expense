3f352ba3af09ce00f0f860dcb004aedd6a6eafff
Sue <choi.soyoung@gmail.com>
2022-04-23 16:45:06 -0700

    195. Using The Custom Hook In More Components

--- src/components/NewTask/NewTask.tsx
+++ src/components/NewTask/NewTask.tsx
@@ -1,44 +1,28 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 import Task from '../../types/Task';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props: {onAddTask: (task: Task) => void}) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
+  const {isLoading, error, sendRequest: sendTaskRequest} = useHttp();
+  
+  const createTask = (taskText: string, taskData: any) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask: Task = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText: string) => {
-    setIsLoading(true);
-    setError('');
-    try {
-      const url = `https://${process.env.REACT_APP_FIREBASE_KEY}.firebaseio.com/tasks.json`;
-      const response = await fetch(url,
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
+    const url = `https://${process.env.REACT_APP_FIREBASE_KEY}.firebaseio.com/tasks.json`;
+    sendTaskRequest({
+      url,
+      method: 'POST',
+      body: { text: taskText },
+      headers: {
+        'Content-Type': 'application/json',
       }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask: Task = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message || 'Something went wrong!');
-      }
-    }
-    setIsLoading(false);
+    }, createTask.bind(null, taskText));
   };
 
   return (
--- src/types/RequestInfo.tsx
+++ src/types/RequestInfo.tsx
@@ -1,6 +1,6 @@
 export default interface RequestInfo {
     method?: string;
     headers?: {};
-    body?: string;
+    body?: string | {};
     url: string;
 }
