2b9c2dd9e6d8d3cebeea44bbe6944bc3595e212b
Sue <choi.soyoung@gmail.com>
2022-04-23 11:48:54 -0700

    190. Configuring Custom Hooks

--- src/hooks/use-counter.tsx
+++ src/hooks/use-counter.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (forwards: boolean = true) => {
     const [counter, setCounter] = useState<number>(0);
 
     useEffect(() => {
-      const interval = setInterval(() => {
-        setCounter((prevCounter) => prevCounter + 1);
-      }, 1000);
+      const interval = setInterval(() => 
+        setCounter((prevCounter) => forwards ? prevCounter + 1 : prevCounter - 1)
+      , 1000);
   
       return () => clearInterval(interval);
-    }, []);
+    }, [forwards]);
 
     return counter;
 };
