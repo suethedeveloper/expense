6e0720e2b542928efc07a92d42a4e731e9ec54a8
Sue <choi.soyoung@gmail.com>
2022-05-01 12:36:56 -0700

    216. Fetching Meals via Http

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,49 +1,51 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-];
+import { useEffect, useState } from "react";
+import MealType from "../../types/MealType";
 
 const AvailableMeals  = () => {
-    const mealsList = DUMMY_MEALS.map((meal: any) => (
-      <MealItem 
-        key={meal.id}
-        id={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />)
-    );
-    return <section className={classes.meals}>
-        <Card>
-            <ul>{mealsList}</ul>
-        </Card>
-    </section>
+  const [meals, setMeals] = useState<MealType[]>([]);
+  /* 
+    should not return a promise. only syncronized function.
+    DO NOT USE THIS WAY: useEffect(async() => { await fetch().then() },[])
+    if you want to return aysnc func then simple create a new func inside which returns promise 
+  */
+  useEffect(() => {
+    const URL = `https://${process.env.REACT_APP_FIREBASE_KEY}.firebaseio.com/meals.json`;
+
+    const fetchMeals = async() => {
+      const response = await fetch(URL, {});
+      const responseData = await response.json();
+      const loadedMals = [];
+      for (const key in responseData) {
+        loadedMals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        })
+      }
+      setMeals(loadedMals);
+    }
+
+    fetchMeals();
+  },[]);
+  
+  const mealsList = meals.map((meal: any) => (
+    <MealItem 
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />)
+  );
+  return <section className={classes.meals}>
+      <Card>
+          <ul>{mealsList}</ul>
+      </Card>
+  </section>
 }
 
 export default AvailableMeals;
