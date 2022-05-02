2a73ff05b4af8a2ed9eb617983b8980217e4bbe0
Sue <choi.soyoung@gmail.com>
2022-05-01 22:23:54 -0700

    212. Using Reducer

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -1,57 +1,58 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
-type Action = {
-    type: 'INPUT' | 'BLUR' | 'RESET';
-    value?: string;
-}
-type State = {
-    value: string;
-    isTouched: boolean;
+interface State {
+  isTouched: boolean;
+  value: string;
 }
 
-type Reducer<State, Action> = 
-  (state: State, action: Action) => State;
-  
+type Action =
+  | { type: "INPUT"; value: string }
+  | { type: "BLUR" }
+  | { type: "RESET" };
+
 const initialInputState = {
-  value: '',
+  value: "",
   isTouched: false,
 };
 
-const inputStateReducer = (state: State, action: Action) => {
-  if (action.type === 'INPUT') {
+const inputStateReducer = (state: State, action: Action): State => {
+  if (action.type === "INPUT") {
     return { value: action.value, isTouched: state.isTouched };
   }
-  if (action.type === 'BLUR') {
+  if (action.type === "BLUR") {
     return { isTouched: true, value: state.value };
   }
-  if (action.type === 'RESET') {
-    return { isTouched: false, value: '' };
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
   }
   return state;
 };
 
-const useInput = (validateValueFn: (value:string) => boolean) => {
-  const [inputState, dispatch] = useReducer<any>(
+const useInput = (validateValue: (arg: string) => boolean) => {
+  //Add useReducer
+  const [inputState, dispatch] = useReducer(
     inputStateReducer,
     initialInputState
   );
-
-  const valueIsValid = validateValueFn(inputState.value);
+  // inputState value가 있으면 valid, isTouched
+  const valueIsValid = validateValue(inputState.value);
   const hasError = !valueIsValid && inputState.isTouched;
 
+  //Add dispatch functions instead of setFunctions
   const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({ type: 'INPUT', value: event.target.value });
+    dispatch({ type: "INPUT", value: event.target.value });
   };
 
   const inputBlurHandler = () => {
-    dispatch({ type: 'BLUR' });
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    dispatch({ type: 'RESET' });
+    dispatch({ type: "RESET" });
   };
 
   return {
+    //value for inputState state
     value: inputState.value,
     isValid: valueIsValid,
     hasError,
