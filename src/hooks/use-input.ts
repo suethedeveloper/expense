93152c397a4bf4d2a752b0472d2f2801c6a8600b
Sue <choi.soyoung@gmail.com>
2022-05-01 22:23:04 -0700

    212. part-1 Using Reducer

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -1,33 +1,64 @@
-import { useState } from "react";
+import { useReducer } from 'react';
+
+type Action = {
+    type: 'INPUT' | 'BLUR' | 'RESET';
+    value?: string;
+}
+type State = {
+    value: string;
+    isTouched: boolean;
+}
+
+type Reducer<State, Action> = 
+  (state: State, action: Action) => State;
+  
+const initialInputState = {
+  value: '',
+  isTouched: false,
+};
+
+const inputStateReducer = (state: State, action: Action) => {
+  if (action.type === 'INPUT') {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === 'BLUR') {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === 'RESET') {
+    return { isTouched: false, value: '' };
+  }
+  return state;
+};
 
 const useInput = (validateValueFn: (value:string) => boolean) => {
-    const [enteredValue, setEnteredValue] = useState<string>("");
-    const [isTouched, setIsTouched] = useState<boolean>(false);
+  const [inputState, dispatch] = useReducer<any>(
+    inputStateReducer,
+    initialInputState
+  );
 
-    const valueIsValid = validateValueFn(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValueFn(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
-    const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEnteredValue(event.target.value);
-      }
-    
-    const inputBlurHandler = () => {
-        setIsTouched(true);
-    }
+  const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'INPUT', value: event.target.value });
+  };
 
-    const reset = () => {
-        setEnteredValue('');
-        setIsTouched(false);
-    }
+  const inputBlurHandler = () => {
+    dispatch({ type: 'BLUR' });
+  };
 
-    return {
-        value: enteredValue,
-        isValid: valueIsValid,
-        hasError,
-        valueChangeHandler,
-        inputBlurHandler,
-        reset
-    }
-}
+  const reset = () => {
+    dispatch({ type: 'RESET' });
+  };
+
+  return {
+    value: inputState.value,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
 
 export default useInput;
