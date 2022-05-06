534962165084d966a9e02c9c12aa0216329c0872
Sue <choi.soyoung@gmail.com>
2022-05-05 20:57:24 -0700

    231. More Redux Basics

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require("redux");
 
 const counterReducer = (state = {counter : 0}, action) => {
-    return {
-        counter: state.counter + 1
-    };
+    if (action.type === "increment") {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === "descrement") {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -11,11 +19,10 @@ const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
     const latestState = store.getState();
-    console.log(latestState);
+    console.log("latestState", latestState);
 };
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({
-    type: 'increment'
-})
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'descrement'});
