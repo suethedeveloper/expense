40497bfc44b16d13d321dc28211ff0f18097353e
Sue <choi.soyoung@gmail.com>
2022-05-10 23:27:23 -0700

    243. Connecting Redux Toolkit State

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,11 @@
 
-import { createSlice } from "@reduxjs/toolkit";
-
-interface State {
-    counter:number;
-    showCounter:boolean
-}
-interface Action {
-    type: string;
-    amount: number;
-}
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 // export const INCREMENT = "increment";
 
 const initialState = {counter: 0, showCounter: true};
-const initialAction = {type: "increment", amount: 0};
-createSlice({
+// const initialAction = {type: "increment", amount: 0};
+const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
@@ -34,39 +25,15 @@ createSlice({
     }
 })
 
-//not working...  action: {type: string, amount?: null | number | undefined
-const counterReducer =
-(state: State = initialState, action: Action = initialAction) => {
-    // if (action.type === INCREMENT) {
-    if (action.type === "increment") {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === "increase") {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === "decrement") {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === "toggle") {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
+//CreatStore only works with one reducer. not idea for a big application. 
+// const store = createStore(counterReducer);
+//configureStore
 
-    return state;
-}
-
-//@ts-ignore
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer //one
+    // reducer: {
+    //     counter: counterSlice.reducer // multiple
+    // }
+})
 
 export default store;
