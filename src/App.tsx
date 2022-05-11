bdf6c0c74aa0c806ad8471f4607f36e2ddba11c8
Sue <choi.soyoung@gmail.com>
2022-05-11 08:22:52 -0700

    246. Reading & Dispatching From A New Slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from "./components/Auth";
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const isAuth = useSelector((state: any) => state.auth.isAuthenticated); //from store/index
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
       <Counter />
+      {isAuth && <UserProfile />}
     </Fragment>
   );
 }
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,10 +1,21 @@
+import { useDispatch } from 'react-redux';
+
 import classes from './Auth.module.css';
+import { authActions } from "../store/index";
+import React from 'react';
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    dispatch(authActions.login());
+  }
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
