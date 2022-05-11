f34b182959dfe85c143c3065e737c6de83868846
Sue <choi.soyoung@gmail.com>
2022-05-11 07:08:20 -0700

    245. Working with Multiple Slices - 1

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,18 @@
 
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-// export const INCREMENT = "increment";
-
+//could do this but logically makes no sense since auth is nothing to do with counter
+//better to create brand new slice
+// const initialState = {counter: 0, showCounter: true, isAuthenticated: false};
 const initialState = {counter: 0, showCounter: true};
 
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
+        // login() {
+
+        // },
         increment(state) {
             state.counter++;
         },
@@ -25,11 +29,28 @@ const counterSlice = createSlice({
     }
 })
 
+const initialAuthState = {
+    isAuthenticated: false
+};
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
 //to get hold of the identifiers use counterSlice.actions
 // counterSlice.actions.toggleCounter
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
 })
 
 export const counterActions = counterSlice.actions; //use on the component where we need actions (counter component)
