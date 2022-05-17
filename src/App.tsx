72bda6753d04037b5660f0d3a6510853d24a2994
Sue <choi.soyoung@gmail.com>
2022-05-17 08:46:01 -0700

    258. A Problem with useEffect()

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useAppSelector((state) => state.ui.cartIsvisible);
+  const cart = useAppSelector((state) => state.cart);
+  const URL = `${process.env.REACT_APP_FIREBASE}/cart.json`;
+
+  useEffect(() => {
+    fetch(URL, {
+      method: "PUT",
+      body: JSON.stringify(cart)
+    })
+  }, [cart]);
 
   return (
     <Layout>
