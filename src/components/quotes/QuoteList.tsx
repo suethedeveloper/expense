5156cd4b48d1aee5f0975edc48ee28862ff3b683
Sue <choi.soyoung@gmail.com>
2022-05-30 22:54:44 -0700

    286. Working with Query Parameters

--- src/components/quotes/QuoteList.tsx
+++ src/components/quotes/QuoteList.tsx
@@ -1,13 +1,44 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import Quote from "./types";
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
-const QuoteList = (props: any) => {
+const sortQuotes = ({quotes}:{quotes: Quote[]}, ascending: boolean) => {
+  return quotes.sort((quoteA: Quote, quoteB: Quote) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  })
+}
+
+const QuoteList = ({quotes}:{quotes: any}) => {
+  // {quotes}:{quotes:Quote[]}
+  const history = useHistory<string []>();
+  const location = useLocation<string>();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+  }
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote: any) => (
+        {sortedQuotes.map((quote: Quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
---
+++ src/components/quotes/types.d.ts
@@ -0,0 +1,5 @@
+export default interface Quote {
+    id: string;
+    text: string;
+    author: string;
+}
