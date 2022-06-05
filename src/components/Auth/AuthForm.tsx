c9f3cdafd63f18d434615758bf88186bb459c447
Sue <choi.soyoung@gmail.com>
2022-06-05 12:39:34 -0700

    304. Adding User Signup

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -1,25 +1,71 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+  
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current?.value;
+    const enteredPassword= passwordInputRef.current?.value;
+
+    //Add validation if wanted
+
+    if (isLogin) {
+
+    } else {
+      const key = process.env.REACT_APP_FIREBASE_WEB_API_KEY as string;
+      const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
+      /** body data payload is from
+       * https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+       */
+
+      //SignUp Request
+      fetch(
+        URL,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then((res: any) => {
+          if (res.ok) {
+            //...
+          } else {
+            return res.json().then((data: any) => {
+              // show an error modal
+              console.log(data);
+            })
+          }
+        });
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passwordInputRef} required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
